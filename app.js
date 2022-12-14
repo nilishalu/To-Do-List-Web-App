8660f2ac05bf484be36a54ab409ce57ba00c9590
nilishalu <shalini.c.e.ganu@gmail.com>
2022-12-14 22:01:12 +0530

    Add simple template

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const express = require("express")
 
 const app = express()
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
     var today = new Date();
     var currDay = today.getDay();
+    var day = "";
 
-    if (currDay == 6 || currDay == 0) {
-        res.write("<p>It's a weekend</p>");
-        res.write("<h1>Let's chill out</h1>")
-        res.send();
-    }
-    else {
-        res.sendFile(__dirname + "/index.html")
-    }
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+    // if (currDay == 6 || currDay == 0) {
+    //     day = "weekend";
+    // }
+    // else {
+    //     day = "weekday"
+    // }
+
+    day = days[currDay];
+
+    res.render('list', {kindOfDay: day});
 })
 
 app.listen(3000, function() {
