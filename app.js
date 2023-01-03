e7e9f14fac5b37d19a5593eb539d8c4606c1f739
nilishalu <shalini.c.e.ganu@gmail.com>
2023-01-03 14:37:29 +0530

    add work list page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const bodyParser = require("body-parser")
 const express = require("express")
+const date = require(__dirname + "/date.js");
 
 const app = express()
 
@@ -8,30 +9,35 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static("public"))
 
-var items = ["Finish WOC responsive web",
+const items = ["Finish WOC responsive web",
 "Finish IC Campus connect work",
 "Complete IRIS Product task work"
 ]
 
+const workItems = [];
+
 app.get('/', function(req, res) {
-    var today = new Date();
+    console.log(date)
+    const day = date.getDate();
 
-    var options = {
-        weekday: 'long',
-        day: 'numeric',
-        month: 'long'
-    }
+    res.render('list', {listTitle: day, newItems: items});
+})
 
-    var day = today.toLocaleDateString("en-US", options)
-
-    res.render('list', {kindOfDay: day, newItems: items});
+app.get("/work", function(req, res) {
+    res.render('list', {listTitle: "Work", newItems: workItems});
 })
 
 app.post('/', function(req, res) {
-    var item = req.body.newItem;
-    items.push(item)
+    const item = req.body.newItem;
 
-    res.redirect("/");
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work")
+    }
+    else {
+        items.push(item)
+        res.redirect("/");
+    }
 })
 
 app.listen(3000, function() {
